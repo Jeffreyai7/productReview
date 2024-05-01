341d100b4313a22a9681e0d89cd755b0dc1f37fc
jeffreyai7 <jirukeh@gmail.com>
2024-05-02 00:35:30 +0100

    picture effected

--- script.js
+++ script.js
@@ -1,17 +1,17 @@
-const productImage = document.getElementById("productImage");
+// const productImage = document.getElementById("productImage");
 
 
-function changingPicture(){
-    const width = window.innerWidth;
-    if(width < 750){
-   productImage.src = "images/image-product-mobile.jpg"
-    }else{
-        productImage.src = "images/image-product-desktop.jpg"
-    }
-}
+// function changingPicture(){
+//     const width = window.innerWidth;
+//     if(width < 750){
+//    productImage.src = "images/image-product-mobile.jpg"
+//     }else{
+//         productImage.src = "images/image-product-desktop.jpg"
+//     }
+// }
 
 
 
-window.addEventListener("resize", ()=>{
-    changingPicture()
-})
+// window.addEventListener("resize", ()=>{
+//     changingPicture()
+// })
